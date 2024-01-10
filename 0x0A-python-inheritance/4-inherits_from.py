31ae680016922a7a11ecb721e74a5e8e7caeb7ce
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-01-10 14:43:08 -0800

    Add inherits_from function for checking class inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-""" A function that return"""
+"""
+module only sub class of
+"""
+
 
 def inherits_from(obj, a_class):
-    """
-    Write a function that returns True if the object is an instance of a class
-    that inherited (directly or indirectly) from the specified class ; otherwise False
+    """returns True if the object is an instance of a class
+       that inherited (directly or indirectly) from the specified class;
+       otherwise False.
     """
     if type(obj) != a_class:
         return issubclass(type(obj), a_class)
