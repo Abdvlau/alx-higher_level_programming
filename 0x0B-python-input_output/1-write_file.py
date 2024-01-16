282e66e1e29821b54217cd66696485a674296763
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-01-16 04:37:48 -0800

    fix indentation error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""Defines a text file written"""
+"""Defines a text-writing function"""
 
 def write_file(filename="", text=""):
     """
     :param filename: Path to the file
-    :param text: String content to write to the file.
+    :param text: String content to write to the file
     :return: Number of characters written
     """
 
     with open(filename, 'w', encoding="utf-8") as f:
-        chars_written = f.write(text)
-        return chars_written
+        return f.write(text)
